f7f0efa7368fc94bcc1345c4534cfe896552e122
Gautam Singh <gautamsingh88081@gmail.com>
2024-06-23 14:19:23 +0530

    I make changes in DB connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDb =async()=>{
    return await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected successfully!'))
+    .then(() => console.log('Something went wrong during MongoDB connection!'))
     .catch(err => console.log(err));
 }
 
