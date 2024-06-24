c9baa76240c2f32700487d759d2cd1aa76ffc9eb
Raju Singh <rajuofficialk@gmail.com>
2024-06-24 13:19:24 +0530

    ..

--- backend/src/Controllers/scenario.js
+++ backend/src/Controllers/scenario.js
@@ -1,4 +1,5 @@
 const Scenario = require('../Model/scenario');
+const scenarioUpdate = require('../Model/scenarioUpdate')
 exports.scenario=async(req,res)=>{
 try{
     const{risk_scenario,risk_description,risk_field1,risk_field2,tags,status,save_as_draft}=req.body;
@@ -31,6 +32,20 @@ try{
         message:`Something went wrong in scenario controller ${err.message}`
     })
 }
+}
+exports.scenarioUpdate=async(req,res)=>{
+    try{
+        const {risk_scenario_id,likehood_score,business_score}=req.body;
+        if(!risk_scenario_id){
+           return res.send("risk scenario is required");
+        }
+       const response=await scenarioUpdate.create({risk_scenario_id:risk_scenario_id,net_score:(likehood_score*business_score)/5})
+       return res.send("scenario updated successfully")
+       }
+       catch(err){
+       return res,send(`oops something gets wrong ${err.message}`)
+   }
+   
 }
 
 exports.getAllScenario=async(req,res)=>{
--- backend/src/Routes/scenario.js
+++ backend/src/Routes/scenario.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { scenario, getAllScenario } = require("../Controllers/scenario");
+const { scenario, getAllScenario,scenarioUpdate } = require("../Controllers/scenario");
 const { auth } = require("../middlewares/auth");
 const router = express.Router();
 // Create scenario route.
 router.post("/scenario/create", auth, scenario);
+router.post("/scenario/update", auth, scenarioUpdate);
 // Get All scenario data route.
 router.get("/scenario", auth, getAllScenario);
 
